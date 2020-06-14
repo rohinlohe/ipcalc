a9bef1fdcf1c39ea415ecb60308dea3ad42d1999
rohinlohe <rohinlohe@users.noreply.github.com>
2020-06-14 15:50:36 -0700

    create a simple cli interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    /// Activate debug mode
+    // short and long flags (-d, --debug) will be deduced from the field's name
+    #[structopt(short, long)]
+    debug: bool,
+
+    /// Pattern to look for
+    pattern: String,
+
+    /// Path to the file
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
 }
